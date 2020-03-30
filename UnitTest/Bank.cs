e946937384ef557d006edfc5d70171e157696d28
bm295 <baominh.nguyen.295@gmail.com>
2020-03-30 21:44:29 +0700

    Add Bank Account Domain

---
+++ UnitTest/Bank.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Bank;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace UnitTest
+{
+    public class Bank
+    {
+        [Fact]
+        public void AccountId_Should_Be_Guid()
+        {
+            var account = new Account();
+            Assert.IsType<Guid>(account.Id);
+        }
+
+        [Fact]        
+        public void AccountBalance_Computation()
+        {
+            var account = new Account
+            {
+                Credits = new List<decimal> { 1500 },
+                Debits = new List<decimal> { 500 }
+            };
+            Assert.Equal(1000, account.Balance);
+        }
+    }
+}
