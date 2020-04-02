a221b190eb8f61030dae9263de8365b27a73ca71
bm295 <baominh.nguyen.295@gmail.com>
2020-04-02 22:21:30 +0700

    Generate runners for datatype performace test

---
+++ SoccerLeague/Implement/Runner/DataTypePerformanceRunner.cs
@@ -0,0 +1,36 @@
+﻿using SoccerLeague.Interface;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace SoccerLeague.Implement.Runner
+{
+    public class DataTypePerformanceRunner : IRunner
+    {
+        public void RunExample()
+        {
+            var listA = new List<int>();
+            var stopWatch = new Stopwatch();
+            
+            stopWatch.Start();
+            for (var i = 0; i < 10000; i++)
+            {
+                listA.Add(i);
+            }
+            stopWatch.Stop();
+            Console.WriteLine($"Using List<int> costs {stopWatch.ElapsedTicks}");
+
+            stopWatch.Reset();
+            var listB = new int[10000];
+
+            stopWatch.Start();
+            for (var i = 0; i < 10000; i++)
+            {
+                listB[i] = i;
+            }
+            stopWatch.Stop();
+            Console.WriteLine($"Using int[] costs {stopWatch.ElapsedTicks}");
+        }
+    }
+}
---
+++ SoccerLeague/Interface/IRunner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoccerLeague.Interface
+{
+    public interface IRunner
+    {
+        void RunExample();
+    }
+}
