1fbdddfb546450caf3bc6ad48519d808c0c8e295
bm295 <baominh.nguyen.295@gmail.com>
2020-08-08 23:31:42 +0700

    Add NeuronSignal interface

---
+++ Domain/Interface/INeuronSignal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interface
+{
+    public interface INeuronSignal
+    {
+        double Output { get; set; }
+    }
+}
